863f9f562011001f1d8e76a111d24e72c16997d9
Ricardo <rich.rios1990@gmail.com>
2025-07-08 14:31:01 -0700

    initial commit

---
+++ back-end/src/temp-data.js
@@ -0,0 +1,43 @@
+export const cartItems = ['123', '345'];
+
+export const products = [{
+  id: '123',
+  name: 'Basil',
+  price: '$26.00',
+  imageURL: '/image/basil-bottle.webp',
+}, {
+  id: '234',
+  name: 'Lemon',
+  price: '$26.00',
+  imageURL: '/image/lemon-bottle.webp',
+}, {
+  id: '345',
+  name: 'Garlic',
+  price: '$26.00',
+  imageURL: '/image/garlic-bottle.webp',
+}, {
+  id: '456',
+  name: 'Rosemary',
+  price: '$26.00',
+  imageURL: '/image/rosemary-bottle.webp',
+}, {
+  id: '567',
+  name: 'Chili',
+  price: '$26.00',
+  imageURL: '/image/chili-bottle.webp',
+}, {
+  id: '678',
+  name: 'Delicate',
+  price: '$24.50',
+  imageURL: '/image/delicate-bottle.webp',
+}, {
+  id: '789',
+  name: 'Medium',
+  price: '$24.50',
+  imageURL: '/image/medium-bottle.webp',
+}, {
+  id: '890',
+  name: 'Bold',
+  price: '$24.50',
+  imageURL: '/image/bold-bottle.webp',
+}];
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList :cartItems="cartItems"/>
+    <button class="checkout-button">Proceed to checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    You have no items in your cart
+  </div>
+</template>
+
+<script>
+import { cartItems } from '../temp-data';
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+
+export default {
+  name: "ShoppingCartPage",
+  components: {
+    ShoppingCartList,
+  },
+  data() {
+    return {
+      cartItems,
+    }
+  }
+}
+</script>
